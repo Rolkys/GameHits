333ff76d1c4a8bd8e1b32528c3a1e0983837de5e
Raul <a@a>
2025-05-20 23:22:09 +0200

    v1.6.1.1

--- GameHits/src/main/java/modelo/Cliente.java
+++ GameHits/src/main/java/modelo/Cliente.java
@@ -29,6 +29,30 @@ public class Cliente  {
         this.gamePoints = gamePoints;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
     public int getGamePoints() {
         return gamePoints;
     }
