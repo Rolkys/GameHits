09058b6a8145044e93acfa653873a7d4473eada6
Raul <a@a>
2025-05-20 23:26:25 +0200

    v1.6.1.3

--- GameHits/src/main/java/modelo/Empleado.java
+++ GameHits/src/main/java/modelo/Empleado.java
@@ -36,6 +36,27 @@ public class Empleado  {
         this.puesto = puesto;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }
